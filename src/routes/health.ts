a714cdfc7a0b4db7d765b1b50193d1f605540d4e
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-17 12:17:05 +0700

    cors and body-parser

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { logger } from "../utils/logger";
 
 export const HealthRouter : Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info("Health Check Success")
   res.status(200).send({ status: '200' })
 })
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response, Router } from 'express'
+import { logger } from '../utils/logger'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Get Product Success')
   res.status(200).send({
     status: true,
     statusCode: 200,
@@ -13,4 +15,13 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
       }
     ]
   })
+})
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Add Product Success')
+   res.status(200).send({
+     status: true,
+     statusCode: 200,
+     data: req.body
+   })
 })

bb4d5588260106dc6a707952f1f0fe0f73be86eb
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-17 16:25:48 +0700

    refactor controller

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { logger } from '../utils/logger'
+import { createProductValidation } from '../validations/product.validation'
+
+export const createProduct = (req: Request, res: Response) => {
+  const { error, value } = createProductValidation(req.body)
+  if (error) {
+    logger.error('ERR = product - create', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+  }
+  logger.info('Add Product Success')
+  return res.status(200).send({
+    status: true,
+    statusCode: 200,
+    message: 'Add product success',
+    data: value
+  })
+}
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import { logger } from '../utils/logger'
-import { createProductValidation } from '../validation/product.validation'
+import { createProduct } from '../controllers/product.controller'
 
 export const ProductRouter: Router = Router()
 
@@ -18,17 +18,4 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   })
 })
 
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = createProductValidation(req.body)
-  if (error) {
-    logger.error('ERR = product - create', error.details[0].message)
-    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
-  }
-  logger.info('Add Product Success')
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    message: 'Add product success',
-    data: value
-  })
-})
+ProductRouter.post('/',createProduct)

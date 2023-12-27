8ef06341f525299b7dfb820ba149e7af0ec092c3
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-27 13:34:54 +0700

    add user validation

---
+++ src/validations/auth.validation.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi'
+import UserType from '../types/user.type'
+
+export const createProductValidation = (payload: UserType) => {
+  const schema = Joi.object({
+    user_id: Joi.string().required(),
+    email: Joi.string().required(),
+    name: Joi.string().required(),
+    password: Joi.string().required(),
+    role: Joi.string().allow('', null)
+  })
+  return schema.validate(payload)
+}
